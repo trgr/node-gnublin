5476d7eebb5737c4d6d2d0178f0b518180188ba1
RampantOwl <trygve@grimnes.org>
2013-12-03 16:11:09 +0000

    Added PorMode,SetAddress, ReadState

--- src/pca9555.cpp
+++ src/pca9555.cpp
@@ -7,9 +7,6 @@ using namespace v8;
 
 Persistent<Function> pca9555::constructor;
 
-pca9555::pca9555(double value) : value_(value) {
-}
-
 pca9555::~pca9555() {
 }
 
@@ -22,9 +19,11 @@ void pca9555::Init(Handle<Object> exports) {
   
   NODE_SET_PROTOTYPE_METHOD(tpl,"PinMode",pca9555::PinMode);
   NODE_SET_PROTOTYPE_METHOD(tpl,"DigitalWrite",pca9555::DigitalWrite);
+  NODE_SET_PROTOTYPE_METHOD(tpl,"DigitalRead",pca9555::DigitalWrite);
+  NODE_SET_PROTOTYPE_METHOD(tpl,"ReadState",pca9555::DigitalWrite);
+  NODE_SET_PROTOTYPE_METHOD(tpl,"SetAddress",pca9555::DigitalWrite);
+  
 
-
-  // Prototype
   constructor = Persistent<Function>::New(tpl->GetFunction());
   exports->Set(String::NewSymbol("pca9555"), constructor);
 }
@@ -47,18 +46,57 @@ Handle<Value> pca9555::DigitalWrite(const Arguments& args){
   int result = obj->pca9555_gnublin->digitalWrite(pin,value);
   return scope.Close(Number::New(result));
 }
+
+Handle<Value> pca9555::DigitalRead(const Arguments& args){
+  HandleScope scope;
+  pca9555* obj = ObjectWrap::Unwrap<pca9555>(args.This());
+  int pin  = args[0]->IntegerValue();
+  int result = obj->pca9555_gnublin->digitalRead(pin);
+  return scope.Close(Number::New(result));
+}
+
+Handle<Value> pca9555::ReadState(const Arguments& args){
+  HandleScope scope;
+  pca9555* obj = ObjectWrap::Unwrap<pca9555>(args.This());
+  int pin  = args[0]->IntegerValue();
+  int result = obj->pca9555_gnublin->readState(pin);
+  return scope.Close(Number::New(result));
+}
+
+void pca9555::SetAddress(const Arguments& args){
+  HandleScope scope;
+  pca9555* obj = ObjectWrap::Unwrap<pca9555>(args.This());
+  int address  = args[0]->IntegerValue();
+  obj->pca9555_gnublin->setAddress(address);
+  return;
+}
+
 Handle<Value> pca9555::PinMode(const Arguments& args){
   HandleScope scope;
   pca9555* obj = ObjectWrap::Unwrap<pca9555>(args.This());
-  int value  = args[0]->IntegerValue();
+  int pin  = args[0]->IntegerValue();
 
   Local<String> v8mode = args[1]->ToString();
   String::Utf8Value umode(v8mode);
   const char* mode(*umode);
   
-  int result = obj->pca9555_gnublin->pinMode(value,mode);
+  int result = obj->pca9555_gnublin->pinMode(pin,mode);
 
   return scope.Close(Number::New(result));
   
+}
+
+Handle<Value> pca9555::PortMode(const Arguments& args){
+  HandleScope scope;
+  pca9555* obj = ObjectWrap::Unwrap<pca9555>(args.This());
+  int port  = args[0]->IntegerValue();
+
+  Local<String> v8mode = args[1]->ToString();
+  String::Utf8Value umode(v8mode);
+  const char* mode(*umode);
+  
+  int result = obj->pca9555_gnublin->portMode(port,mode);
+
+  return scope.Close(Number::New(result));  
 }
 
--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,6 @@ var gpio = new gnublin.pca9555();
 
 assert.equal(gpio.PinMode(1,"out"),1,"SetPin 1 out fail")
 assert.equal(gpio.PinMode(4,"out"),-1,"SetPin 4 out success") //Should fail according to gnublin.cpp
-
 assert.equal(gpio.DigitalWrite(1,1),1,"Could not set high");
 
 
