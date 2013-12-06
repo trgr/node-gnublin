ed00c4c4d1821ff41c4b0f00f03bea63b4e38ec9
RampantOwl <trygve@grimnes.org>
2013-12-06 18:11:33 +0000

    Added INPUT/OUTPUT HIGH/LOW as properties to base object

--- src/node-gnublin.cpp
+++ src/node-gnublin.cpp
@@ -4,8 +4,25 @@
 
 using namespace v8;
 
+Handle<Value> CreateObject(const Arguments& args) {
+  HandleScope scope;
+  
+  Local<Object> obj = Object::New();
+  obj->Set(String::NewSymbol("INPUT"), String::New("in"));
+  
+  return scope.Close(obj);
+}
+
 void InitAll(Handle<Object> exports) {
   pca9555::Init(exports);
+  
+  /* Expos some properties matching constants found in gnublin.cpp
+     for convenience
+   */
+  exports->Set(String::NewSymbol("INPUT"), String::New(INPUT));
+  exports->Set(String::NewSymbol("OUTPUT"),String::New(OUTPUT));
+  exports->Set(String::NewSymbol("HIGH"),  Number::New(HIGH));
+  exports->Set(String::NewSymbol("LOW"),   Number::New(LOW));
 }
 
 NODE_MODULE(gnublin, InitAll)
